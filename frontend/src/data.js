ff2a727ff41c396d2cb6b165d9d206f8e74c326e
Barry <Barry-B15@users.noreply.github.com>
2020-11-03 12:38:17 +0900

    setup: convert static to react app, +dynamic links

---
+++ frontend/src/data.js
@@ -0,0 +1,89 @@
+const data = {
+    products: [{
+            _id: '1',
+            name: 'Zini Slim Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 120,
+            brand: 'Zini',
+            rating: 4.5,
+            numReviews: 10,
+            descrition: 'high quality product'
+        },
+
+        {
+            _id: '2',
+            name: 'Zini BB Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 320,
+            brand: 'Zini',
+            rating: 4.3,
+            numReviews: 10,
+            descrition: 'high quality product'
+        },
+
+        {
+            _id: '3',
+            name: 'Zini Oxford Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 120,
+            brand: 'Zini',
+            rating: 4.0,
+            numReviews: 10,
+            descrition: 'high quality product'
+        },
+
+        {
+            _id: '4',
+            name: 'Zini Showman Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 120,
+            brand: 'Zini',
+            rating: 4.5,
+            numReviews: 10,
+            descrition: 'high quality product'
+        },
+
+        {
+            _id: '5',
+            name: 'Zini College Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 120,
+            brand: 'Zini',
+            rating: 4.5,
+            numReviews: 10,
+            descrition: 'high quality product'
+        },
+
+        {
+            _id: '6',
+            name: 'Zini Gents Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 120,
+            brand: 'Zini',
+            rating: 4.5,
+            numReviews: 10,
+            descrition: 'high quality product'
+        },
+
+        {
+            _id: '7',
+            name: 'Zini Biz Shirts',
+            category: 'Shirts',
+            image: '/images/slm1.jpg',
+            price: 450,
+            brand: 'Zini',
+            rating: 5.0,
+            numReviews: 100,
+            descrition: 'high quality product'
+        },
+
+    ],
+};
+
+export default data;
